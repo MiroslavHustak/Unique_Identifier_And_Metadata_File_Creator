7860e401cfcad28b2b2e699a650af2e8e53c6ca9
Miroslav Husťák <97535187+MiroslavHustak@users.noreply.github.com>
2023-01-28 04:07:09 +0100

    28-01-2023

--- Unique_Identifier_And_Metadata_File_Creator.Models/MyTypes/Strings.fs
+++ Unique_Identifier_And_Metadata_File_Creator.Models/MyTypes/Strings.fs
@@ -2,13 +2,11 @@
 
 open System
 
-let private myOption s = 
+let (|StringNonN|) s = 
     s 
     |> Option.ofObj 
     |> function 
         | Some value -> string value
         | None       -> String.Empty
-
-let (|StringNonN|) s = myOption s 
 
 //let private myString (StringNonN s) = s //to be implemented in the relevant module
